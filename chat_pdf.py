f04ccdcc83ed4d16b96847a7981c5c54706a4e28
Nima <nimaalighardashi@gmail.com>
2024-09-30 02:22:06 +0330

    added the basic of st and tempfile

--- chat_pdf.py
+++ chat_pdf.py
@@ -9,10 +9,10 @@ from streamlit_chat import message
 def embedchain_bot(db_path):
     return App.from_config(
         config={
-            "llm": {"provider": "ollama", "config": {"model": "llama3.2:latest", "max_tokens":300, 
-                     "temprature": 0.5, "stram": True, "base_url": "http://localhost:11434"}},
+            "llm": {"provider": "ollama", "config": {"model": "llama3.2:latest", "max_tokens":250, 
+                     "temperature": 0.5, "stream": True, "base_url": "http://localhost:11434"}},
             "vectordb": {"provider": "chroma", "config": {"dir": db_path}},
-            "embedder": {"provider": "ollama", "config": {"model": "llama3.2:lastest", "base_url": "http://localhost:11434"}},
+            "embedder": {"provider": "ollama", "config": {"model": "llama3.2:latest", "base_url": "http://localhost:11434"}},
         }
     )
 
@@ -20,3 +20,12 @@ def display_pdf(file):
     base64_pdf = base64.b64encode(file.read()).decode('utf-8')
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="400" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
+
+st.title("PersoBot")
+st.caption("This is a personal chatbot that works without internet. it's based on llama3.2:latest")
+
+db_path = tempfile.mktemp()
+if 'app' not in st.session_state:
+    st.session_state.app = embedchain_bot(db_path)
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
